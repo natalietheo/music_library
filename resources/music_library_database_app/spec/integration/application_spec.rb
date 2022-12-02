aff35f96e82023361791820287d4e2f3571d3e61
Natalie Theodoropoulou <natalie.theo@gmail.com>
2022-12-02 14:17:48 +0000

    end of phase 2 challenges

--- resources/music_library_database_app/spec/integration/application_spec.rb
+++ resources/music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,55 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "GET /albums" do
+    it 'should return the list of albums' do
+      # Assuming the post with id 1 exists.
+      response = get('/albums')
+      expected_response = 'Doolittle, Surfer Rosa, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings,Waterloo, Mezzanine'
+
+      expect(response.status).to eq(200)
+      # expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context 'POST /albums' do
+    it 'should create a new album' do
+      response = post('/albums', 
+        title: 'OK Computer', 
+        release_year: '1997', 
+        artist_id: '1')
+      expect(response.status).to eq(200)
+      expect(response.body).to eq ('')
+
+      response = get('/albums')
+
+      expect(response.body).to include('OK Computer')
+
+    end
+  end
+
+ context 'GET /artists' do
+    it 'gets a list of all artists' do
+
+     response = get('/artists') 
+
+    expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
+
+    expect(response.status).to eq(200)
+    expect(response.body).to eq(expected_response)
+end
+end
+
+context 'POST /artists' do
+  it 'should create a new artist' do
+    response = post('/artists', name: 'Wild nothing', genre: 'Indie')
+    expect(response.status).to eq(200)
+    expect(response.body).to eq ('')
+
+    response = get('/artists')
+
+    expect(response.body).to include('Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos, Wild nothing')
+    end
+  end
+
 end
